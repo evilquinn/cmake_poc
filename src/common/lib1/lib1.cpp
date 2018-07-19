1ff34402e9e1da0062d5e79b2ddc58885825a5b2
evilquinn <evilquinn@gmail.com>
2018-07-19 23:42:55 +0100

    vaguely what i want

---
+++ src/common/lib1/lib1.cpp
@@ -0,0 +1,8 @@
+
+#include <iostream>
+#include <common/lib1/lib1.hpp>
+
+lib1_class::lib1_class()
+{
+    std::cout << "lib1_class()" << std::endl;
+}
---
+++ src/common/lib1/lib1.hpp
@@ -0,0 +1,11 @@
+
+#ifndef SRC_COMMON_LIB1_HPP
+#define SRC_COMMON_LIB1_HPP
+
+class lib1_class
+{
+public:
+    lib1_class();
+};
+
+#endif // SRC_COMMON_LIB1_HPP
